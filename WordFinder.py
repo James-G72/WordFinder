bcce9d4eaf839c82db6db5a97e6509c679acb463
James Gower <66065325+James-G72@users.noreply.github.com>
2021-10-19 16:59:32 +0100

    Can now print definitions based on order search

--- WordFinder.py
+++ WordFinder.py
@@ -1,32 +1,24 @@
-def indent_print(list):
+def indent_print(line):
     indent = "   "
-    
-    for line in list:
-        try:
-            list = meaning_list[type]
-            print(indent+type+":")
-            for defs in list:
-                print(indent+indent+defs)
-
-        except:
-            pass
+    word = line.split("::")[0]
+    print(word)
+    defs = line.replace(word+"::","").split("__")[1:]
+    for line in defs:
+        print(indent+line.replace("\n",""))
 
 # Asking for parameters
 order = input("Use hyphens and letters to describe known letter locations. Example: letter would be --t-e-")
 length = len(order)
 known_letters = order.replace("-","")
+keys = input("Keyword(s): ")
 
 if order == known_letters:
-    blockPrint()
-    definition = dictionary.meaning(order)
-    enablePrint()
-    print(order)
-    if definition == None:
-        print("No definition known")
-    else:
-        indent_print(definition)
-    print("\n")
-    exit()
+    with open("MeaningsOneLine.txt","r") as f:
+        for line in f:
+            word = line.split("::")[0]
+            if word == order:
+                indent_print(line)
+                quit()
 
 def containsAll(str, set):
     for c in set:
@@ -35,10 +27,11 @@ def containsAll(str, set):
     return True
 
 word_list = []
-with open("every_word.txt","r") as f:
+with open("MeaningsOneLine.txt","r") as f:
     for line in f:
-        if len(line) == length + 1:
-            if containsAll(line, known_letters):
+        word = line.split("::")[0]
+        if len(word) == length:
+            if containsAll(word, known_letters):
                 word_list.append(line)
 
 order_count = 0
@@ -47,21 +40,14 @@ for char in order:
     if char != "-":
         check_possition = 0
         while check_possition < len(word_list):
-            if word_list[check_possition][order_count] != char:
+            if word_list[check_possition].split("::")[0][order_count] != char:
                 word_list.remove(word_list[check_possition])
             else:
                 check_possition += 1
     order_count += 1
 
 for printer in word_list:
-    blockPrint()
-    definition = dictionary.meaning(printer.replace("\n",""))
-    enablePrint()
-    print(printer.replace("\n",""))
-    if definition == None:
-        print("No definition known")
-    else:
-        indent_print(definition)
+    indent_print(printer)
     print("\n")
 
 
